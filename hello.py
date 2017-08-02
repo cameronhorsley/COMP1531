1958be700237f561856afce2e9c5f66b385f5f1d
Cameron Horsley <z5075556@zmail.unsw.edu.au>
2017-08-02 10:51:37 +1000

    fizz to 100

--- hello.py
+++ hello.py
@@ -1,2 +1,21 @@
-print("hello world")
-print("line 2")
+#!/usr/bin/python
+
+import sys
+
+print("Hello, git!")
+
+#if (len(sys.argv) != 2):
+#	print ("Usage: %s <integer>" % sys.argv[0])
+#	sys.exit(1)
+	
+#n = int(sys.argv[1])
+
+for x in range(1, 101) :
+	if (x % 3 == 0 and x % 5 == 0) :
+		print("FIZZ BUZZ")
+	elif (x % 3 == 0) :
+		print("FIZZ")
+	elif (x % 5 == 0) :
+		print("BUZZ")
+	else :
+		print(x)
